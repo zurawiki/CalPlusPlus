96fe7189704ba6933cae060d10c28407b63ad403
Roger Zurawicki <rzurawicki@gmail.com>
2013-05-02 05:18:44 -0400

    added debug info to classifier

--- lib/stuff-classifier/base.rb
+++ lib/stuff-classifier/base.rb
@@ -109,7 +109,7 @@ class StuffClassifier::Base
     @category_list[category][:_count] ? @category_list[category][:_count].to_f : 0.0
   end
 
-  # return the number of time categories in wich a word appear
+  # return the number of time categories in which a word appear
   def categories_with_word_count(word)
     return 0 unless @word_list[word] && @word_list[word][:categories]
     @word_list[word][:categories].length
@@ -127,17 +127,21 @@ class StuffClassifier::Base
 
   # train the classifier
   def train(category, text)
+    logger.warn "Training event of text #{text} \n into category #{category}"
     @tokenizer.each_word(text) { |w| increase_word(w, category) }
     increase_category(category)
+    logger.warn "words_in_cat|cat_doc_count\n#{total_word_count(category)}|#{category_count(category)}"
   end
 
   # classify a text
   def classify(text, default=nil)
+    logger.warn "Classifying event of text #{text}"
     # Find the category with the highest probability
     max_prob = @min_prob
     best = nil
 
     scores = category_scores(text)
+    logger.warn "Category scores are: #{scores}"
     scores.each do |score|
       cat, prob = score
       if prob > max_prob
