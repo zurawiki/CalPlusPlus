b74983522556ebb29e91202f00319cceabca693f
Tianyu Liu <poohlty@sina.com>
2013-05-02 05:34:23 -0400

    change base.rb

--- lib/stuff-classifier/base.rb
+++ lib/stuff-classifier/base.rb
@@ -126,21 +126,21 @@ class StuffClassifier::Base
   end
 
   # train the classifier
-  def train(category, text)
-    puts "Training event of text #{text} \n into category #{category}"
-    @tokenizer.tokenize(text) { |w| increase_word(w, category) }
+  def train(category, event)
+    puts "Training event of text #{event} \n into category #{category}"
+    @tokenizer.tokenize(event) { |w| increase_word(w, category) }
     increase_category(category)
     puts "words_in_cat|cat_doc_count\n#{total_word_count(category)}|#{category_count(category)}"
   end
 
   # classify a text
-  def classify(text, default=nil)
-    puts "Classifying event of text #{text}"
+  def classify(event, default=nil)
+    puts "Classifying event of text #{event}"
     # Find the category with the highest probability
     max_prob = @min_prob
     best = nil
 
-    scores = category_scores(text)
+    scores = category_scores(event)
     puts "Category scores are: #{scores}"
     scores.each do |score|
       cat, prob = score
